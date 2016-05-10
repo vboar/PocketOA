6042cee118cb3dbedb32be77cc93d751f81e3500
vboar <vboar@live.com>
2016-05-10 18:12:22 +0800

    add customer

--- app/src/main/java/top/kass/pocketoa/ui/AddCustomerActivity.java
+++ app/src/main/java/top/kass/pocketoa/ui/AddCustomerActivity.java
@@ -3,6 +3,8 @@ package top.kass.pocketoa.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 
 import top.kass.pocketoa.R;
@@ -21,6 +23,25 @@ public class AddCustomerActivity extends AppCompatActivity {
                 finish();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_add_customer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == R.id.action_submit) {
+            finish();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
 }
